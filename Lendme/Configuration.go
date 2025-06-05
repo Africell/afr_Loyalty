7b6a2958adeb4e33da5911254002cb670defd69d
eboutross <83222744+eboutross@users.noreply.github.com>
2025-06-05 14:28:23 +0300

    New IN IP

--- Lendme/Configuration.go
+++ Lendme/Configuration.go
@@ -96,7 +96,8 @@ type ConfigType struct {
 func GetDefaultConfiguration() (err error) {
 	//Configuration = setDefaultConfiguration_DRC_Live()
 	//Configuration = setDefaultConfiguration_GM_Live()
-	Configuration = setDefaultConfiguration_SL_Live()
+	//Configuration = setDefaultConfiguration_SL_Live()
+	Configuration = setDefaultConfiguration_DRC_Live_NewIN()
 	return nil
 }
 
@@ -174,6 +175,109 @@ func setDefaultConfiguration_DRC_Live() (Configuration ConfigType) {
 
 	Configuration.IN.IP = "10.70.1.38"
 	Configuration.IN.Port = "8080"
+	Configuration.IN.WS_SOAP_Endpoint = "/axis2/services/WebService.WebServiceHttpSoap12Endpoint/"
+	Configuration.IN.WS_XMLNS_SOAP_Env = "http://schemas.xmlsoap.org/soap/envelope/"
+	Configuration.IN.WS_XMLNS_Web = "http://webservice.CSI.omvia.convergys.com"
+
+	Configuration.IN.WS_EVC_SOAP_Endpoint = "/axis2/services/ERechargeWebService.ERechargeWebServiceHttpSoap11Endpoint/"
+	Configuration.IN.WS_EVC_XMLNS_SOAP_Env = "http://schemas.xmlsoap.org/soap/envelope/"
+	Configuration.IN.WS_EVC_XMLNS_Web = "http://webservice.CSI.omvia.convergys.com"
+
+	Configuration.IN.Default_OpId = "lendme"
+	Configuration.IN.Default_OpPwd = ""
+	Configuration.IN.Is_OpPwd_Required = false
+	Configuration.IN.Timeout = 5
+	Configuration.IN.PrintLogs = false
+
+	//http://10.95.64.6:15403/?systemid=lendme&password=lendmeP@ssw0rd&Originator=setest&dest_addr=243900100606&msg_text=test&registered_delivery=0&ston=5&snpi=0&dton=1&dnpi=1&encoding=1
+
+	//SMPP
+	Configuration.SMPP.IP = "10.95.64.6" //// floating IS IP: "10.250.8.53", test IP VPN: "10.250.0.52" (or .50, .51)
+	Configuration.SMPP.Port = "15403"
+	Configuration.SMPP.Login = "lendme"
+	Configuration.SMPP.Password = "lendmeP@ssw0rd"
+
+	return
+}
+
+func setDefaultConfiguration_DRC_Live_NewIN() (Configuration ConfigType) {
+	//Configuration.HttpOKAPIServicePort = "9291"
+	Configuration.HttpAppServicePort = "9290"
+
+	Configuration.OKAPIAllowedOrigins = append(Configuration.OKAPIAllowedOrigins, "http://localhost:3000")
+	Configuration.OKAPIAllowedOrigins = append(Configuration.OKAPIAllowedOrigins, "http://localhost:5173")
+	Configuration.OKAPIAllowedOrigins = append(Configuration.OKAPIAllowedOrigins, "http://localhost:4173")
+	Configuration.OKAPIAllowedOrigins = append(Configuration.OKAPIAllowedOrigins, "http://localhost:4414")
+	Configuration.OKAPIAllowedOrigins = append(Configuration.OKAPIAllowedOrigins, "https://okpaihr.africell.ao")
+	Configuration.OKAPIAllowedOrigins = append(Configuration.OKAPIAllowedOrigins, "https://outlet.africell.ao")
+
+	Configuration.Operation = "DRC"
+	Configuration.HostId = "Lendme-01"
+	Configuration.DB_Name = "Lendme_DB"
+
+	Configuration.Version = "V1"
+	Configuration.Module = "Lendme"
+
+	Configuration.IsProduction = false
+	Configuration.Min_Allowed_Amnt = 10
+	Configuration.Service_FeePerc = 0.1
+	Configuration.Min_Allowed_AON = 3
+	Configuration.Min_Avg3MRecharge = 50
+	Configuration.Min_LastRechargePeriod = 60
+	Configuration.Min_Allowed_Balance = 0
+	Configuration.Max_Allowed_Balance = 1000
+	Configuration.ARPU_File_Path = "/home/Subs_ARPU/"
+
+	Configuration.App_AUC.Description = "App AUC service"
+	Configuration.App_AUC.Protocol = "http"
+	Configuration.App_AUC.Hostname = "Lendme_auc"
+	Configuration.App_AUC.Port = "9001"
+	Configuration.App_AUC.Module = "AUC"
+	Configuration.App_AUC.Version = "V1"
+	Configuration.App_AUC.S2S_Username = "Lendme_Admin"
+	Configuration.App_AUC.S2S_Password = "s@l$e$IrSW0$4"
+	Configuration.App_AUC.Timeout_After = 5 * time.Second
+
+	// Configuration.OKAPI_AUC.Description = "OKAPI AUC service"
+	// Configuration.OKAPI_AUC.Protocol = "https"
+	// Configuration.OKAPI_AUC.Hostname = "auc"
+	// Configuration.OKAPI_AUC.Port = "9001"
+	// Configuration.OKAPI_AUC.Module = "AUC"
+	// Configuration.OKAPI_AUC.Version = "V1"
+	// Configuration.OKAPI_AUC.S2S_Username = "SalesMonitoring_OKAPI"
+	// Configuration.OKAPI_AUC.S2S_Password = "s@le$P@s$W0$3"
+	// Configuration.OKAPI_AUC.Timeout_After = 5 * time.Second
+
+	//mongoDB
+	// Configuration.MongoDB.ReplicaSet = ""
+	// Configuration.MongoDB.UserName = "db_root"
+	// Configuration.MongoDB.Password = "B3202T@soSo0612w6"
+	// Configuration.MongoDB.HostIP_1 = "LendMe_mongodb"
+	// Configuration.MongoDB.HostPort_1 = "27017"
+	// Configuration.MongoDB.HostIP_2 = ""
+	// Configuration.MongoDB.HostPort_2 = ""
+	// Configuration.MongoDB.HostIP_3 = ""
+	// Configuration.MongoDB.HostPort_3 = ""
+	// Configuration.MongoDB.HostIP_4 = ""
+	// Configuration.MongoDB.HostPort_4 = ""
+	Configuration.MongoDB.ReplicaSet = "reps1"
+	Configuration.MongoDB.UserName = "db_root"
+	Configuration.MongoDB.Password = "B3202T@soSo0612w6"
+	Configuration.MongoDB.HostIP_1 = "10.95.64.177" //==> Primary
+	Configuration.MongoDB.HostPort_1 = "9001"
+	Configuration.MongoDB.HostIP_2 = "10.95.64.176" //==>secondary
+	Configuration.MongoDB.HostPort_2 = "9002"
+	Configuration.MongoDB.HostIP_3 = "10.95.32.22" //==> Aribter
+	Configuration.MongoDB.HostPort_3 = "9003"
+	Configuration.MongoDB.HostIP_4 = ""
+	Configuration.MongoDB.HostPort_4 = ""
+
+	// Configuration.IN.IP = "10.70.1.38"
+	// Configuration.IN.Port = "8080"
+
+	Configuration.IN.IP = "10.95.73.12" //"10.70.1.59"
+	Configuration.IN.Port = "8444"
+
 	Configuration.IN.WS_SOAP_Endpoint = "/axis2/services/WebService.WebServiceHttpSoap12Endpoint/"
 	Configuration.IN.WS_XMLNS_SOAP_Env = "http://schemas.xmlsoap.org/soap/envelope/"
 	Configuration.IN.WS_XMLNS_Web = "http://webservice.CSI.omvia.convergys.com"
