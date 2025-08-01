b35a80788ef3a9f61a5175bea58dcf45e7f02ff4
AhmadElSaid-Africell <aelsaid@africell.com>
2025-08-01 16:08:20 +0300

    Update

--- Lendme/Configuration.go
+++ Lendme/Configuration.go
@@ -199,7 +199,7 @@ type ConfigType struct {
 
 func GetDefaultConfiguration() (err error) {
 	// Configuration = setDefaultConfiguration_DRC_Live()
-	// Configuration = setDefaultConfiguration_DRC_Loyalty_UAT()
+	Configuration = setDefaultConfiguration_DRC_Loyalty_UAT()
 	//	Configuration = setDefaultConfiguration_GM_Live()
 
 	// Configuration = setDefaultConfiguration_SL_Live()
@@ -209,7 +209,7 @@ func GetDefaultConfiguration() (err error) {
 	// Configuration = setDefaultConfiguration_AO_Loyalty()
 	//Configuration = setDefaultConfiguration_AO_Loyalty_UAT()
 
-	Configuration = setDefaultConfiguration_GM_Loyalty_Live()
+	// Configuration = setDefaultConfiguration_GM_Loyalty_Live()
 	// Configuration = setDefaultConfiguration_GM_Loyalty_UAT()
 	return nil
 }
@@ -497,7 +497,7 @@ func setDefaultConfiguration_DRC_Loyalty_UAT() (Configuration ConfigType) {
 	Configuration.SMPP.Encoding = 0
 	//CGW
 	Configuration.CGW.Protocol = "http"
-	Configuration.CGW.Hostname = "UCGW"
+	Configuration.CGW.Hostname = "10.95.72.177"
 	Configuration.CGW.Port = "9991"
 	Configuration.CGW.Module = "UCGW"
 	Configuration.CGW.Version = "V1"
@@ -506,8 +506,8 @@ func setDefaultConfiguration_DRC_Loyalty_UAT() (Configuration ConfigType) {
 
 	Configuration.CGW_AUC.Description = "UCGW AUC service"
 	Configuration.CGW_AUC.Protocol = "http"
-	Configuration.CGW_AUC.Hostname = "UCGW_AUC"
-	Configuration.CGW_AUC.Port = "9001"
+	Configuration.CGW_AUC.Hostname = "10.95.72.177"
+	Configuration.CGW_AUC.Port = "9994"
 	Configuration.CGW_AUC.Module = "AUC"
 	Configuration.CGW_AUC.Version = "V1"
 	Configuration.CGW_AUC.S2S_Username = "UCGW_Admin"
@@ -524,21 +524,21 @@ func setDefaultConfiguration_DRC_Loyalty_UAT() (Configuration ConfigType) {
 	Configuration.Propylaea.S2S_Password = "uC@g$W$iRiS6$2@333dd"
 	Configuration.Propylaea.Timeout_After = 5 * time.Second
 
-	Configuration.SpinAndWin_AUC.Description = "SAW AUC"
-	Configuration.SpinAndWin_AUC.Protocol = "http"
-	Configuration.SpinAndWin_AUC.Hostname = "10.95.64.166"
-	Configuration.SpinAndWin_AUC.Port = "9001"
-	Configuration.SpinAndWin_AUC.Module = "AUC"
-	Configuration.SpinAndWin_AUC.Version = "V1"
-	Configuration.SpinAndWin_AUC.S2S_Username = "SAW_Admin"
-	Configuration.SpinAndWin_AUC.S2S_Password = "LQaDUp388UNKhz0Ap"
-	Configuration.SpinAndWin_AUC.Timeout_After = 30 * time.Second
-	Configuration.SpinAndWin.Protocol = "http"
-	Configuration.SpinAndWin.Hostname = "10.95.64.166"
-	Configuration.SpinAndWin.Port = "9111"
-	Configuration.SpinAndWin.Module = "SpinAndWin"
-	Configuration.SpinAndWin.Version = "V1"
- 	Configuration.SpinAndWin.Timeout = 30 * time.Second
+	// Configuration.SpinAndWin_AUC.Description = "SAW AUC"
+	// Configuration.SpinAndWin_AUC.Protocol = "http"
+	// Configuration.SpinAndWin_AUC.Hostname = "10.95.72.166"
+	// Configuration.SpinAndWin_AUC.Port = "9001"
+	// Configuration.SpinAndWin_AUC.Module = "AUC"
+	// Configuration.SpinAndWin_AUC.Version = "V1"
+	// Configuration.SpinAndWin_AUC.S2S_Username = "SAW_Admin"
+	// Configuration.SpinAndWin_AUC.S2S_Password = "LQaDUp388UNKhz0Ap"
+	// Configuration.SpinAndWin_AUC.Timeout_After = 30 * time.Second
+	// Configuration.SpinAndWin.Protocol = "http"
+	// Configuration.SpinAndWin.Hostname = "10.95.72.166"
+	// Configuration.SpinAndWin.Port = "9111"
+	// Configuration.SpinAndWin.Module = "SpinAndWin"
+	// Configuration.SpinAndWin.Version = "V1"
+ 	// Configuration.SpinAndWin.Timeout = 30 * time.Second
  
 
 	return
