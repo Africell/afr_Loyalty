4b6beb6eb85a9dd6d1560c0f0189a2a3ba352734
AA-Zein <aalzein@africell.com>
2026-01-23 10:48:38 +0200

    Update Routes_Definition_all.go

--- Lendme/Routes_Definition_all.go
+++ Lendme/Routes_Definition_all.go
@@ -771,6 +771,28 @@ func (UC *UserControl) Add_LoyaltyServiceRoutes(R *Routes) {
 		true,               //AllowedFor_App
 	}
 	*R = append(*R, r)
+	Level1 = "Loyalty Level"
+	Level1DisplayOrder = Level1DisplayOrder + 1
+	r = Route{
+		"HTTP_Loyalty_Level",
+		"GET",
+		"/" + Configuration.LoyaltyModule + "/" + Configuration.LoyaltyVersion + "/HTTP_Loyalty_Level/",
+		Use(UC.HTTP_Loyalty_Level, UC.ValidateAccess_AUC, UC.ValidateJWEToken),
+		true,
+		"Loyalty Level - Read", // DisplayName
+		1,                      // DisplayOrder
+		Module,                 // Module
+		ModuleDisplayOrder,     //ModuleDisplayOrder
+		Level1,                 // Level1
+		Level1DisplayOrder,     // Level1DisplayOrder
+		"",                     // Level2
+		0,                      // Level2DisplayOrder
+		"",                     // Level3
+		0,                      // Level3DisplayOrder
+		true,                   //AllowedFor_OKAPI
+		true,                   //AllowedFor_App
+	}
+	*R = append(*R, r)
 
 }
 
