620a51fd0839325d68bf7abd3873c43fdd26626e
hadi-tarhinii <hmtarhini@africell.com>
2026-06-03 10:37:45 +0300

    increment counters only when redeeming

--- Lendme/Loyalty_Functions.go
+++ Lendme/Loyalty_Functions.go
@@ -9034,6 +9034,12 @@ func (Uc *UserControl) Loyalty_AccountDebitPoints(request_header *Request_Header
 						putCancel()
 					}
 					if Amount_to_debit == 0 {
+						if !notRedemptionValue {
+							loyalty_Account.Daily_Redeemed_Points += request.Debit_Amount
+							loyalty_Account.Weekly_Redeemed_Points += request.Debit_Amount
+							loyalty_Account.Daily_Redemption_Attempts++
+							loyalty_Account.Weekly_Redemption_Attempts++
+						}
 						break
 					}
 				}
@@ -9043,12 +9049,6 @@ func (Uc *UserControl) Loyalty_AccountDebitPoints(request_header *Request_Header
 		} else {
 			break
 		}
-	}
-	if !notRedemptionValue {
-		loyalty_Account.Daily_Redeemed_Points += request.Debit_Amount
-		loyalty_Account.Weekly_Redeemed_Points += request.Debit_Amount
-		loyalty_Account.Daily_Redemption_Attempts++
-		loyalty_Account.Weekly_Redemption_Attempts++
 	}
 	{
 		putCtx, putCancel := context.WithTimeout(context.Background(), 10*time.Second)
